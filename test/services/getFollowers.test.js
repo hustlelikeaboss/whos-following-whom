24333b8f35ff714785b49ef1f40b3f4c11dc3245
Quan Jin <quan.jin@meridinet.com>
2018-09-27 15:01:56 -0500

    updated tests

--- test/services/getFollowers.test.js
+++ test/services/getFollowers.test.js
@@ -9,8 +9,9 @@ chai.use(chaiHttp);
 const server = require('../../server');
 const getFollowers = require('../../services/getFollowers');
 const response = require('../data/hustlelikeaboss.followers');
+
 /**
- * TEST ENDPOINT: /ping
+ * TEST  getFollowers.byUsername()
  */
 describe('TEST getFollowers.byUsername("hustlelikeaboss")', () => {
 
@@ -24,12 +25,51 @@ describe('TEST getFollowers.byUsername("hustlelikeaboss")', () => {
     it('should return an array of 3 followers with status code 200', (done) => {
          getFollowers.byUsername('hustlelikeaboss')
             .then(followers => {
-                followers.should.be.a('array');
+                followers.should.be.an('array');
                 followers.should.have.lengthOf(3);
             });
 
         done();
     });
 
+
+});
+
+/**
+ * TEST getFollowers.forArrayOfUsers()
+ */
+describe('TEST getFollowers.forArrayOfUsers()', () => {
+
+    beforeEach(() => {
+        nock('https://api.github.com')
+            .get('/users/beyhosni/followers')
+            .reply(200, response[0].theirFollowers);
+
+        nock('https://api.github.com')
+            .get('/users/brittanymwagner/followers')
+            .reply(200, response[1].theirFollowers);
+
+        nock('https://api.github.com')
+            .get('/users/ryhan000/followers')
+            .reply(200, response[2].theirFollowers);
+    });
+
+
+    it('should return an array of 3 users and their followers with status code 200', (done) => {
+        let users = [
+            {"followers_url": "https://api.github.com/users/beyhosni/followers"},
+            {"followers_url": "https://api.github.com/users/brittanymwagner/followers"},
+            {"followers_url": "https://api.github.com/users/ryhan000/followers"}
+        ]
+
+        let unresolvedPromises = getFollowers.forArrayOfUsers(users);
+        unresolvedPromises.should.be.an('array').that.has.lengthOf(3);
+        unresolvedPromises[0].should.be.a('promise');
+
+        let resolvedUsers = Promise.all(unresolvedPromises);
+        done();
+        resolvedUsers.should.be.an('array').that.has.lengthOf(3);
+        resolvedUsers[0].followers.should.have.lengthOf(5);
+    });
 
 });
